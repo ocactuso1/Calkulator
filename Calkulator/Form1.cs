95da229ea48db1e16e4f54117e9700e83a728e2e
Karina Volkova <misshelin1983@gmail.com>
2022-10-23 15:02:58 +0300

    переделка

--- Calkulator/Form1.cs
+++ Calkulator/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calkulator
@@ -15,101 +8,180 @@ namespace Calkulator
         public Form1()
         {
             InitializeComponent();
+
+            textBox1.Text = " ";
         }
 
-        double a, b;         //тип с плавающей точкой
-        int count;          //целочисленный тип
-        bool znak = true;   //меняет знак числа ( true — положительное число,false — отрицательное число)
+        public double number;//число
+        public char action = ' ';//действие которое мы будем выполнять
+        bool znak = true;//меняет знак числа ( true — положительное число,false — отрицательное число)
+        bool numberCheck;//проверяет какое число мы сейчас заполняем
+        bool tockaCheck;//проверяет на точку
 
         private void button0_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 2;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 3;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 4;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 5;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 6;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 7;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 8;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()) && numberCheck)
+            {
+                DeleteText();
+            }
+
             textBox1.Text = textBox1.Text + 9;
         }
 
         private void buttonTochka_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text.IndexOf(",") == -1)
+            if (textBox1.Text.IndexOf(",") == -1 && tockaCheck == false)
             {
                 textBox1.Text += ",";
+
+                tockaCheck = true;
             }
         }
 
         private void buttonResalt_Click(object sender, EventArgs e)
         {
             tcalculate();
-            textBox1.Text = "";
         }
 
-        private void tcalculate()
+        public void tcalculate()
         {
-            switch (count)
+            if ("x/+-".Contains(textBox1.Text[textBox1.Text.Length - 1].ToString()))
             {
-                case 1:
-                    b = a + double.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
-                case 2:
-                    b = a - double.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
-                case 3:
-                    b = a * double.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
-                case 4:
-                    b = a / double.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
-                case 5:
-                    b = a % double.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
-                    break;
+                return;
+            }
 
+            switch (action)
+            {
+                case '+':
+                    number += double.Parse(textBox1.Text);
+                    textBox1.Text = number.ToString();
+                    numberCheck = false;
+                    tockaCheck = false;
+                    znak = true;
+                    action = ' ';
+                    break;
+                case '-':
+                    number -= double.Parse(textBox1.Text);
+                    textBox1.Text = number.ToString();
+                    numberCheck = false;
+                    tockaCheck = false;
+                    znak = true;
+                    action = ' ';
+                    break;
+                case 'x':
+                    number *= double.Parse(textBox1.Text);
+                    textBox1.Text = number.ToString();
+                    numberCheck = false;
+                    tockaCheck = false;
+                    znak = true;
+                    action = ' ';
+                    break;
+                case '/':
+                    number /= double.Parse(textBox1.Text);
+                    textBox1.Text = number.ToString();
+                    numberCheck = false;
+                    tockaCheck = false;
+                    znak = true;
+                    action = ' ';
+                    break;
+                case '%':
+                    number %= double.Parse(textBox1.Text);
+                    textBox1.Text = number.ToString();
+                    numberCheck = false;
+                    tockaCheck = false;
+                    znak = true;
+                    action = ' ';
+                    break;
                 default:
                     break;
             }
@@ -117,68 +189,83 @@ namespace Calkulator
 
         private void buttonPlas_Click(object sender, EventArgs e)
         {
-            if (CheckActionForRepeat(textBox1.Text))
+            if (CheckActionForRepeat() && ActionIsNotFirst())
             {
-                a = double.Parse(textBox1.Text);
-                count = 1;
-                textBox1.Text += '+';
-                znak = true;
+                if (numberCheck == false)
+                {
+                    AddNumber();
+                    action = '+';
+                    textBox1.Text += '+';
+                    znak = true;
+                    numberCheck = true;
+                }
             }
         }
 
         private void buttonMinus_Click(object sender, EventArgs e)
         {
-            if (CheckActionForRepeat(textBox1.Text))
+            if (CheckActionForRepeat() && ActionIsNotFirst())
             {
-                a = double.Parse(textBox1.Text);
-                count = 2;
-                textBox1.Text += '-';
-                znak = true;
+                if (numberCheck == false)
+                {
+                    AddNumber();
+                    action = '-';
+                    textBox1.Text += '-';
+                    znak = true;
+                    numberCheck = true;
+                }
             }
         }
 
         private void buttonMnojit_Click(object sender, EventArgs e)
         {
-            if (CheckActionForRepeat(textBox1.Text))
+            if (CheckActionForRepeat() && ActionIsNotFirst())
             {
-                a = double.Parse(textBox1.Text);
-                count = 3;
-                textBox1.Text += 'x';
-                znak = true;
+                if (numberCheck == false)
+                {
+                    AddNumber();
+                    action = 'x';
+                    textBox1.Text += 'x';
+                    znak = true;
+                    numberCheck = true;
+                }
             }
         }
 
         private void buttonDiliti_Click(object sender, EventArgs e)
         {
-            if (CheckActionForRepeat(textBox1.Text))
+            if (CheckActionForRepeat() && ActionIsNotFirst())
             {
-                a = double.Parse(textBox1.Text);
-                count = 4;
-                textBox1.Text += '/';
-                znak = true;
+                if (numberCheck == false)
+                {
+                    AddNumber();
+                    action = '/';
+                    textBox1.Text += '/';
+                    znak = true;
+                    numberCheck = true;
+                }
             }
         }
-         
+
         private void buttonZnak_Click(object sender, EventArgs e)
         {
             if (znak == true)
             {
-                textBox1.Text = "-" + textBox1.Text;
+                textBox1.Text = "-" + textBox1.Text.Trim();
                 znak = false;
             }
             else if (znak == false)
             {
-                textBox1.Text = textBox1.Text.Replace("-", "");
+                textBox1.Text = textBox1.Text.Trim().Replace("-", "");
                 znak = true;
             }
         }
 
         private void buttonPresent_Click(object sender, EventArgs e)
         {
-            if (CheckActionForRepeat(textBox1.Text))
+            if (CheckActionForRepeat() && ActionIsNotFirst())
             {
-                a = double.Parse(textBox1.Text);
-                count = 5;
+                number = double.Parse(textBox1.Text);
                 textBox1.Text += '%';
                 znak = true;
             }
@@ -186,34 +273,58 @@ namespace Calkulator
 
         private void buttonDelete_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "";
-            label1.Text = "";
+            textBox1.Text = " ";
+            number = 0;
+            numberCheck = false;
+            tockaCheck = false;
+            znak = true;
+            action = ' ';
         }
 
-        private bool CheckActionForRepeat(string text)
+        private bool ActionIsNotFirst()
         {
-            if(text.Contains("-"))
+            if (char.IsDigit(textBox1.Text[textBox1.Text.Length - 1]))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckActionForRepeat()
+        {
+            if (action == '-')
             {
                 return false;
             }
-            if (text.Contains("+"))
+            if (action == '+')
             {
                 return false;
             }
-            if (text.Contains("x"))
+            if (action == 'x')
             {
                 return false;
             }
-            if (text.Contains("/"))
+            if (action == '/')
             {
                 return false;
             }
-            if (text.Contains("%"))
+            if (action == '%')
             {
                 return false;
             }
 
             return true;
         }
-    }  
+
+        private void AddNumber()
+        {
+            number = double.Parse(textBox1.Text);
+        }
+
+        private void DeleteText()
+        {
+            textBox1.Clear();
+        }
+    }
 }
